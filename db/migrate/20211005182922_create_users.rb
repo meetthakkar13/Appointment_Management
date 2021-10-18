cc37c0961f5b0026d07e97962d1f9025ccfac7aa
meetthakkar13 <mj.thakkar97@gmail.com>
2021-10-18 10:47:17 +0530

    removed files

--- db/migrate/20211005182922_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :users do |t|
-      t.belongs_to :role, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20211005211455_add_ref_to_doctors.rb
+++
@@ -1,8 +0,0 @@
-class AddRefToDoctors < ActiveRecord::Migration[6.1]
-  def change
-    change_table :users do |t|
-      t.references :doctors, foreign_key: { to_table: :users }
-  
-    end
-  end
-end
